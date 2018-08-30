c84ccdd28f3c27cfc521ab591d1f2102fb7f149b
jcuevas <jmoics@gmail.com>
2018-08-30 16:09:00 -0500

    Add Appender and Logger interfaces.

---
+++ src/main/java/pe/exam/appender/Appender.java
@@ -0,0 +1,7 @@
+package pe.exam.appender;
+
+
+public interface Appender
+{
+    void execute(String _message, int _logType);
+}
---
+++ src/main/java/pe/exam/logger/Logger.java
@@ -0,0 +1,9 @@
+package pe.exam.logger;
+
+
+public interface Logger
+{
+    void error(String _message);
+    void warn(String _message);
+    void message(String _message);
+}
